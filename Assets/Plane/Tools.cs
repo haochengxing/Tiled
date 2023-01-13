5693215e943c64d58f6e4f4121530a8e535ad867
LONGTU\chengxinghao <haochengxing2006@163.com>
2023-01-13 10:44:49 +0800

    透视相机下屏幕和地面的4个点

--- Assets/Plane/Tools.cs
+++ Assets/Plane/Tools.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Tools : MonoBehaviour
 {
 
+    public GameObject plane;
+
     public Camera mCamera;
     public GameObject cube0;
     public GameObject cube1;
@@ -14,14 +16,33 @@ public class Tools : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GetPosition();
+        //GetPosition();
+
+        //DrawRay();
+
+        GetPerspectivePosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetPosition();
+        //GetPosition();
+
+        //DrawRay();
+
+        GetPerspectivePosition();
     }
+
+
+    private void GetPerspectivePosition()
+    {
+        cube0.transform.position = CalcScreenToPlanePositon(mCamera, 0, 0, 0);
+        cube1.transform.position = CalcScreenToPlanePositon(mCamera, 1, 0, 0);
+        cube2.transform.position = CalcScreenToPlanePositon(mCamera, 1, 1, 0);
+        cube3.transform.position = CalcScreenToPlanePositon(mCamera, 0, 1, 0);
+
+    }
+
 
     private void GetPosition()
     {
@@ -80,5 +101,52 @@ public class Tools : MonoBehaviour
     {
         Vector3 screenPos = new Vector3(camera.pixelWidth * x, camera.pixelHeight * y, camera.nearClipPlane);
         return CalcPlanePosition(camera, screenPos, planeHeight);
+    }
+
+
+
+    private void DrawRay()
+    {
+
+        var plane = new Plane(this.plane.transform.up, this.plane.transform.position);
+
+
+        var leftButtom = new Vector3(0, 0, 12);
+        var leftTop = new Vector3(0, Screen.height, 12);
+        var rightTop = new Vector3(Screen.width, Screen.height, 12);
+        var rightButtom = new Vector3(Screen.width, 0, 12);
+
+        var ray_leftButtom = mCamera.ScreenPointToRay(leftButtom);
+        var ray_leftTop = mCamera.ScreenPointToRay(leftTop);
+        var ray_rightTop = mCamera.ScreenPointToRay(rightTop);
+        var ray_rightButtom = mCamera.ScreenPointToRay(rightButtom);
+
+        Debug.DrawRay(ray_leftButtom.origin, ray_leftButtom.direction, Color.white, 1f);
+        Debug.DrawRay(ray_leftTop.origin, ray_leftTop.direction, Color.white, 1f);
+        Debug.DrawRay(ray_rightTop.origin, ray_rightTop.direction, Color.white, 1f);
+        Debug.DrawRay(ray_rightButtom.origin, ray_rightButtom.direction, Color.white, 1f);
+
+        float enter = 0;
+
+        if (plane.Raycast(ray_leftButtom, out enter))
+        {
+            cube0.transform.position = ray_leftButtom.GetPoint(enter);
+        }
+
+        if (plane.Raycast(ray_leftTop, out enter))
+        {
+            cube1.transform.position = ray_leftTop.GetPoint(enter);
+        }
+
+        if (plane.Raycast(ray_rightTop, out enter))
+        {
+            cube2.transform.position = ray_rightTop.GetPoint(enter);
+        }
+
+        if (plane.Raycast(ray_rightButtom, out enter))
+        {
+            cube3.transform.position = ray_rightButtom.GetPoint(enter);
+        }
+
     }
 }

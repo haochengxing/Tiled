0c18442c2a4068c2349546b2c89accc2ae66b156
LONGTU\chengxinghao <haochengxing2006@163.com>
2023-02-06 17:34:49 +0800

    行为树

---
+++ Assets/BTTree/Code/Game.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Game : MonoBehaviour {
+
+	private Transform _orcTrans;
+	private Transform _goblinTrans;
+
+	void Update () {
+		int mouseButton = -1;
+		if (Input.GetMouseButtonDown(0)) {
+			mouseButton = 0;
+		}
+		else if (Input.GetMouseButtonDown(1)) {
+			mouseButton = 1;
+		}
+
+		if (mouseButton != -1) {
+			Vector3 mousePosition =  Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			mousePosition.z = 0;
+
+			if (mouseButton == 0) {
+				_goblinTrans = _goblinTrans == null ? (GameObject.Instantiate(Resources.Load("Goblin")) as GameObject).transform : _goblinTrans;
+				_goblinTrans.name = "Goblin";
+				_goblinTrans.transform.position = mousePosition;
+			}
+			else if (mouseButton == 1) {
+				_orcTrans = _orcTrans == null ? (GameObject.Instantiate(Resources.Load("Orc")) as GameObject).transform : _orcTrans;
+				_orcTrans.name = "Orc";
+				_orcTrans.transform.position = mousePosition;
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Space)) {
+			SceneManager.LoadScene(0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+	}
+}
